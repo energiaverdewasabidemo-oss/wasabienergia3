2b2f15665a612f7a050e3a2778461f1ac8a0ec95
energiaverdewasabidemo-oss <energiaverdewasabidemo@gmail.com>
2026-03-04 19:10:55 +0100

    Updated index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react';
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import OptimizedHeader from './components/OptimizedHeader';
 import OptimizedHero from './components/OptimizedHero';
